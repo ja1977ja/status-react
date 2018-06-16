db60e200f7d3f9811e2c99753b5e67b1d742bdf3
Goran Jovic <goranjovic@gmail.com>
2018-06-16 13:59:07 +0200

    token balance in send asset picker

--- src/status_im/chat/views/api/choose_asset.cljs
+++ src/status_im/chat/views/api/choose_asset.cljs
@@ -3,25 +3,33 @@
   (:require [re-frame.core :as re-frame]
             [status-im.ui.components.list.views :as list]
             [status-im.ui.components.react :as react]
-            [status-im.ui.components.colors :as colors]))
+            [status-im.ui.components.colors :as colors]
+            [status-im.utils.money :as money]))
 
 (defn- render-asset [arg-index bot-db-key]
-  (fn [asset]
+  (fn [{:keys [name symbol amount decimals] :as asset}]
     [react/touchable-highlight {:on-press #(re-frame/dispatch
                                             [:set-asset-as-command-argument {:arg-index  arg-index
                                                                              :bot-db-key bot-db-key
-                                                                             :asset      asset}])}
+                                                                             :asset      (dissoc asset :amount)}])}
      [react/view {:flex-direction   :row
                   :align-items      :center
+                  :justify-content  :space-between
                   :padding-vertical 11}
-      [react/image {:source (-> asset :icon :source)
-                    :style  {:width 30
-                             :height 30
-                             :margin-left 14
-                             :margin-right 12}}]
-      [react/text {:style {:color :black}} (:symbol asset)]
-      [react/text {} " "]
-      [react/text {:style {:color :gray}} (:name asset)]]]))
+      [react/view {:flex            1
+                   :flex-direction  :row
+                   :align-items     :center}
+       [react/image {:source (-> asset :icon :source)
+                     :style  {:width        30
+                              :height       30
+                              :margin-left  14
+                              :margin-right 12}}]
+       [react/text {:style {:color :black}} symbol]
+       [react/text {:style {:color        :gray
+                            :padding-left 4}} name]]
+      [react/text {:style {:color         :gray
+                           :padding-right 14}}
+       (str (money/internal->formatted amount symbol decimals))]]]))
 
 (def assets-separator [react/view {:height           1
                                    :background-color colors/gray-light
@@ -29,7 +37,7 @@
 
 (defview choose-asset-view [{arg-index  :index
                              bot-db-key :bot-db-key}]
-  (letsubs [assets [:wallet/visible-assets]]
+  (letsubs [assets [:wallet/visible-assets-with-amount]]
     [react/view
      [list/flat-list {:data                      assets
                       :key-fn                    (comp name :symbol)
